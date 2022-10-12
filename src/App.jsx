b796383acd6982572a6d0af41b7e93ff40ff8b89
dalya111 <dalyaa0803@gmail.com>
2022-10-13 03:19:17 +1100

    set up auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Routes, Route
-} from "react-router-dom"
-import './App.css';
-import NavBar from './components/NavBar';
-import HomePage from './pages/HomePage';
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
-import SearchPage from './pages/SearchPage';
-import { CartProvider } from './hooks/useCart';
+import SearchPage from "./pages/SearchPage";
+import { CartProvider } from "./hooks/useCart";
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const App = ()=>{
-
+const App = () => {
   // const [user, setUser] = useState(null);
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+    useAuth0();
 
-  return (
-    <div>
-      <Router>
-        <CartProvider>
-          <NavBar/>
-          <Routes>
-            <Route path="/search" element={<SearchPage/>} />
-            <Route path="/products/:id" element={<ProductPage/>} />
-            <Route path="/" element={<HomePage/>} />
-          </Routes>
-        </CartProvider>
-      </Router>
-    </div>
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
 
-  )
-}
+  if (isAuthenticated) {
+    return (
+      <div>
+        Hello {user.name}{" "}
+        <button onClick={() => logout({ returnTo: window.location.origin })}>
+          Log out
+        </button>
+        <Router>
+          <CartProvider>
+            <NavBar />
+            <Routes>
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/products/:id" element={<ProductPage />} />
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </CartProvider>
+        </Router>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <button onClick={loginWithRedirect}>Log in</button>
+        <Router>
+          <CartProvider>
+            <NavBar />
+            <Routes>
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/products/:id" element={<ProductPage />} />
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </CartProvider>
+        </Router>
+      </div>
+    );
+  }
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from "./App";
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="dev-9zuuus2t.us.auth0.com"
+    clientId="InRJoXFdFycXxHKaHyTU55hUmrYIyXM4"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
